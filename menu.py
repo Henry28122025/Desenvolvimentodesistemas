12689ead1a7fd0b5914998587ac7e62f1a7a963e
henrysilveira-eng <henry.silveira@aluno.senai.br>
2026-06-25 19:43:58 -0300

    menu

---
+++ menu.py
@@ -0,0 +1,14 @@
+menu = [
+    "Usuario",
+    "etc",
+    "sla"
+]
+
+largura = 30
+
+print("+" + ("-" * largura) + "+")
+
+for item in menu:
+    print(f'|{item:<{largura}}|')
+    
+    print("+" + ("-" * largura) + "+")
